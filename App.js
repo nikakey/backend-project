d9a3ebce4e80695395859a26384c8a7a54452f78
Veronika Kochetkova <veronica90k@gmail.com>
2018-09-22 14:27:47 -0700

    Create db connection

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 const express = require ('express');
 const cors = require ('cors');
+const mysql = require ('mysql');
 
 const app = express();
 const port = 5000;
 
+const connection = mysql.createConnection({
+  host: 'itgbackendhiring.mysql.database.azure.com',
+  user: 'backendhire@itgbackendhiring',
+  password: 'CoughSyrup123!',
+  database: 'itghiringdb'
+})
+
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
