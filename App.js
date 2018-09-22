fdeb2a5387aa48fcf19829b9340d45d24c7fc6de
Veronika Kochetkova <veronica90k@gmail.com>
2018-09-22 15:56:29 -0700

    Enable body-parser

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const cors = require ('cors');
 const mysql = require ('mysql');
 const { body, validationResult } = require('express-validator/check');
+const bodyParser = require ('body-parser');
 
 const app = express();
 const port = 5000;
