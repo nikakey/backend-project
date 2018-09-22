76e2617d446d752b700d06a6b9c7c8b1b603e6e0
Veronika Kochetkova <veronica90k@gmail.com>
2018-09-22 14:16:45 -0700

    Enable CORS

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 const express = require ('express');
+const cors = require ('cors');
 
 const app = express();
 const port = 5000;
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
